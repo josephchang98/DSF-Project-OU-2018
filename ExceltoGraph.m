a12feedfc066d835b24123c5e507231a898eed9a
Bailey McCarty <bailey.c.mccarty@ou.edu>
2018-11-06 19:20:13 -0600

    upload changes

--- ExceltoGraph.m
+++
@@ -1,8 +0,0 @@
-[v,T,vT]=xlsread('Bitcoin.xls') 
-% 'xlsx' for exell 2007
-%v: Double
-%T and vT : cell
-%use v containing numbers 
-t=v(:,1);y=v(:,2)
-%if u have to plot second colone depending on first:
-plot(t,y)
