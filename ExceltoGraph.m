2a8f13a3d840353c34d12388645efe3ed7d022c1
josephchang98 <32658356+josephchang98@users.noreply.github.com>
2018-11-06 19:34:19 -0600

    wasn't grabbing numeric data; fixed it

--- ExceltoGraph.m
+++ ExceltoGraph.m
@@ -1,4 +1,5 @@
-[num, text] = xlsread('Bitcoin', 'A2:A3036');
+[hi,text] = xlsread('Bitcoin', 'A2:A3036');
+[num] = xlsread('Bitcoin', 'B2:B3036');
 plot(num)
 %change formatting for time/date so that all range is correct
 set(gca,'xticklabel', text.')
