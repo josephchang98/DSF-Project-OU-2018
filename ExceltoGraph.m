a9a3d8b420be3b8eba286a6792bd1862de3c6921
Bailey McCarty <bailey.c.mccarty@ou.edu>
2018-11-06 19:41:15 -0600

    Added Comments to ExceltoGraph
    Adding comments to better understand code

--- ExceltoGraph.m
+++ ExceltoGraph.m
@@ -1,5 +1,9 @@
+%takes excel sheet and converts it to data
 [hi,text] = xlsread('Bitcoin', 'A2:A3036');
 [num] = xlsread('Bitcoin', 'B2:B3036');
+
+%plots data
 plot(num)
+
 %change formatting for time/date so that all range is correct
 set(gca,'xticklabel', text.')
