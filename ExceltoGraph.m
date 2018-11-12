a2ec407f453ee66de6985c57ac098db1013fdb2f
josephchang98 <32658356+josephchang98@users.noreply.github.com>
2018-11-12 04:49:12 -0600

    combined two lines to make code cleaner

--- ExceltoGraph.m
+++ ExceltoGraph.m
@@ -1,6 +1,5 @@
 %takes excel sheet and converts it to data
-[hi,text] = xlsread('Bitcoin', 'A2:A3036');
-[num] = xlsread('Bitcoin', 'B2:B3036');
+[num,text] = xlsread('Bitcoin', 'B2:B3036:A2:A3036');
 
 %plots data
 plot(num)
