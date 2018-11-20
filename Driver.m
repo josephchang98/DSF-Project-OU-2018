fb1348c03c89f1febfd6b9551901fd65057824ee
josephchang98 <32658356+josephchang98@users.noreply.github.com>
2018-11-20 16:43:04 -0600

    made multiple figures for original and filtered data, put plotting on driver instead of in functions

--- Driver.m
+++ Driver.m
@@ -1,2 +1,10 @@
 [num, text] = ExceltoGraph('Bitcoin.xlsx');
+%plots data
+figure(1)
+plot(num)
+%change formatting for time/date so that all range is correct
+%set(gca,'xticklabel', text.')
+%xlim auto
 
+figure(2)
+Filtered
