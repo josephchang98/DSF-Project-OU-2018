43b24f52d92c45c6c95b893ab7b0ed7a0469985e
josephchang98 <32658356+josephchang98@users.noreply.github.com>
2018-11-27 10:10:16 -0600

    added comments to make code more readable

--- Filtered.m
+++ Filtered.m
@@ -1,7 +1,9 @@
 function [out] = Filtered(in) 
-Array=in;
-Price = Array(:, 1);
-n = 50;
-yy = filter(ones(1, n) / n, 1, Price);
-out = yy;
+    Array = in;
+    Price = Array(:, 1);
+    
+    % averaging filter; averages every 50 data points
+    n = 50;
+    yy = filter(ones(1, n) / n, 1, Price);
+    out = yy;
 end
